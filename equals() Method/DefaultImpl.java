ccadd79902809b4dcc46bb507ac13531804e01c2
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-11-23 22:39:12 +0530

    Update DefaultImpl.java

--- equals() Method/DefaultImpl.java
+++ equals() Method/DefaultImpl.java
@@ -10,18 +10,18 @@ class Demo1
 }
 public class DefaultImpl
 {
-		  public static void main(String[] args) 
-      {
-    			Demo1 d1 = new Demo1();
-    			Demo1 d2 = new Demo1();
+	public static void main(String[] args) 
+      	{
+    		Demo1 d1 = new Demo1();
+    		Demo1 d2 = new Demo1();
     			
-    			// using == operator
-    			System.out.println(d1==d2);
+		// using == operator
+    		System.out.println(d1==d2);
     			
-    			// using equals() method
-    			System.out.println(d1.equals(d2));
-    			System.out.println(d2.equals(d1));
-		  }
+    		// using equals() method
+    		System.out.println(d1.equals(d2));
+    		System.out.println(d2.equals(d1));
+	}
   }
 
 
