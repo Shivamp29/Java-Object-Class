2cf584ec8e219f2b699750d97618bd9d1b54724c
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-11-23 22:38:15 +0530

    Create DefaultImpl.java

---
+++ equals() Method/DefaultImpl.java
@@ -0,0 +1,27 @@
+/*
+
+  1. Default Implementaion of euals method
+  
+*/
+
+class Demo1 
+{
+	
+}
+public class DefaultImpl
+{
+		  public static void main(String[] args) 
+      {
+    			Demo1 d1 = new Demo1();
+    			Demo1 d2 = new Demo1();
+    			
+    			// using == operator
+    			System.out.println(d1==d2);
+    			
+    			// using equals() method
+    			System.out.println(d1.equals(d2));
+    			System.out.println(d2.equals(d1));
+		  }
+  }
+
+
